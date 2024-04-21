4bee26b5f055f23ddf57857cfde4286717b1919f
Luissa <ulieluissa@gmail.com>
2024-04-21 20:49:04 +0700

    feat: album and photos

---
+++ src/modules/user/album/index.tsx
@@ -0,0 +1,69 @@
+import { Avatar, Image, List, Modal, ModalProps, Select, Spin, Typography } from "antd";
+import { useEffect, useState } from "react";
+import { getData } from "../../../lib";
+
+const { Title } = Typography;
+
+interface Props {
+  modalProps?: ModalProps;
+  user: any;
+}
+
+export function DetailAlbum(props: Props) {
+  const { modalProps = {}, user } = props;
+  const [loading, setLoading] = useState(false);
+  const [albumOptions, setAlbumOptions] = useState([]);
+  const [photos, setPhotos] = useState([]);
+
+  async function generateDetailAlbum(user: any) {
+    setPhotos([]);
+    setLoading(true);
+    const data = await getData({ url: `/albums` });
+    const filteredData = data?.filter((item: any) => item?.userId === user?.id);
+    const options = filteredData?.map((item: any) => {
+      return {
+        label: item?.title,
+        value: item?.id,
+      }
+    })
+    setAlbumOptions(options);
+    setLoading(false);
+  }
+
+  async function handleGeneratePhoto(selected: any) {
+    setLoading(true);
+    setPhotos([]);
+    const data = await getData({ url: `/photos` });
+    const filteredData = data?.filter((item: any) => item?.albumId === selected);
+    setPhotos(filteredData);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    generateDetailAlbum(user);
+  }, [modalProps?.open]);
+
+  return (
+    <Modal {...modalProps} title={`Album ${user?.name}`} footer={[]} width={700} destroyOnClose>
+      <Spin spinning={loading}>
+        <Select onChange={handleGeneratePhoto} style={{ width: '100%' }} options={albumOptions} placeholder="Select Album" />
+      </Spin>
+      {photos?.length > 0 && (
+         <List
+            grid={{ gutter: 16, column: 4 }}
+            dataSource={photos}
+            renderItem={(item: any) => (
+              <List.Item>
+                <Image
+                  src={item?.url}
+                  preview={{
+                    src: item?.thumbnailUrl,
+                  }}
+                />
+              </List.Item>
+            )}
+          />
+      )}
+    </Modal>
+  );
+}
--- src/modules/user/index.tsx
+++ src/modules/user/index.tsx
@@ -1,35 +1,63 @@
-import { Avatar, List as AntdList, Button } from "antd";
+import { Avatar, List as AntdList, Button, ModalProps } from "antd";
 import { List } from "../../components";
 import { useRecoilState } from "recoil";
 import { userState } from "../../lib";
 import { CameraOutlined } from "@ant-design/icons";
+import { useState } from "react";
+import { DetailAlbum } from "./album";
 
 export function User() {
   const [, setUser] = useRecoilState<any>(userState);
+  const [showDetailAlbum, setShowDetailAlbum] = useState(false);
+  const [selectedUser, setSelectedUser] = useState<any>(null);
+  function onCancelModal(e?: any) {
+    e.preventDefault();
+    togglePost(false)
+  }
+
+  function togglePost(show: boolean) {
+    setShowDetailAlbum(show);
+  }
+  
+  const modalProps: ModalProps = {
+    open: showDetailAlbum,
+    onCancel: onCancelModal,
+  }
+
   return (
-    <List
-      listProps={{
-        style: {
-          maxHeight: '75vh',
-          overflowY: 'auto'
-        },
-      }}
-      callbackData={(data) => setUser(data)}
-      modulConfiguration={{
-        title: 'User',
-        apiUrl: 'users',
-      }}
-      renderContent={({ data, index }) => {
-        return (
-          <div style={{ display: 'flex', width: '100%' }}>
-            <AntdList.Item.Meta
-              avatar={<Avatar src={`https://api.dicebear.com/7.x/miniavs/svg?seed=${index}`} />}
-              title={data?.name}
-            />
-            <Button size="small" icon={<CameraOutlined />} />
-          </div>
-        );
-      }}
-    />
+    <>
+      <List
+        listProps={{
+          style: {
+            maxHeight: '75vh',
+            overflowY: 'auto'
+          },
+        }}
+        callbackData={(data) => setUser(data)}
+        modulConfiguration={{
+          title: 'User',
+          apiUrl: 'users',
+        }}
+        renderContent={({ data, index }) => {
+          return (
+            <div style={{ display: 'flex', width: '100%' }}>
+              <AntdList.Item.Meta
+                avatar={<Avatar src={`https://api.dicebear.com/7.x/miniavs/svg?seed=${index}`} />}
+                title={data?.name}
+              />
+              <Button
+                size="small"
+                onClick={() => {
+                  setSelectedUser(data)
+                  togglePost(true)
+                }}
+                icon={<CameraOutlined />}
+              />
+            </div>
+          );
+        }}
+      />
+      <DetailAlbum modalProps={modalProps} user={selectedUser} />
+    </>
   );
 }
